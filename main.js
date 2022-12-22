0fc980eca89da5193ae5b1eac1a4d7837089588e
kesavaprasad08 <kesav2661998@gmail.com>
2022-12-23 01:16:00 +0530

    Lets Scale the app to more users

--- main.js
+++ main.js
@@ -23,26 +23,76 @@ function onclick(e) {
 btn.style.background = "blue";
 }
 
+/*function onSubmit(e) {
+    e.preventDefault();
+    
+    if (nameInput.value === "" || emailInput.value === "") {
+     msg.classList.add("error");
+    
+        msg.innerHTML = "please enter all fields";
+    
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+    var myobj = {
+        name : nameInput.value,
+         email: emailInput.value
+    }
+    var myobjserialized =JSON.stringify(myobj);
+    console.log(myobjserialized);
+    
+    localStorage.setItem('myobj',myobjserialized);
+    
+    }
+    nameInput.value = "";
+    emailInput.value = "";
+    }
+*/ 
 function onSubmit(e) {
-e.preventDefault();
+    e.preventDefault();
+    
+    if (nameInput.value === "" || emailInput.value === "") {
+     msg.classList.add("error");
+    
+        msg.innerHTML = "please enter all fields";
+    
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+    var myobj = {
+        name : nameInput.value,
+         email: emailInput.value
+    }
+    var myobjserialized =JSON.stringify(myobj);
+    console.log(myobjserialized);
+    
+    localStorage.setItem(emailInput.value,myobjserialized);
+    
+    }
+    
+    var btn = document.createElement('button');
+    btn.setAttribute('id','listOfAppointments');
+    var btnDel = document.createElement('button');
+    btn.setAttribute('id','listOfAppointments');
 
-if (nameInput.value === "" || emailInput.value === "") {
- msg.classList.add("error");
+    var ul =document.createElement('ul');
+    ul.setAttribute('id','listOfAppointments');
+    ul.setAttribute('class','items')
+    document.body.appendChild(ul);
+    var li = document.createElement('li');
+    li.setAttribute('class','items')
 
-    msg.innerHTML = "please enter all fields";
+    ul.appendChild(li);
+    ul.appendChild(btn);
+    ul.appendChild(btnDel);
 
-    setTimeout(() => msg.remove(), 3000);
-} else {
-var myobj = {
-    name : nameInput.value,
-     email: emailInput.value
-}
-var myobjserialized =JSON.stringify(myobj);
-console.log(myobjserialized);
+    btn.textContent='Edit';
+    btnDel.textContent='Delete';
+ 
+    li.textContent=nameInput.value + ' ' +emailInput.value;
 
-localStorage.setItem('myobj',myobjserialized);
+    nameInput.value = "";
+    emailInput.value = "";
 
-}
-nameInput.value = "";
-emailInput.value = "";
-}
+
+    }
+    
+    
